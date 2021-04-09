c108d0f1119d40158eb925237d2045045f02c4b9
maxcom24 <45498990+maxcom24@users.noreply.github.com>
2021-04-09 14:56:12 +0000

    Update train.py

--- train.py
+++ train.py
@@ -36,6 +36,7 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+    return x_df,y_df
     
 
 # TODO: Create TabularDataset using TabularDatasetFactory
